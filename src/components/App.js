acb6b344eba365f016be35300a2e9893b833b8a1
flan the dev <flanthedev@gmail.com>
2017-10-24 10:39:12 -0600

    intial commit, set up dependences and API key

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import SearchBar from './SearchBar';
+
+export default class App extends Component {
+  render() {
+    return (
+    <SearchBar />
+    )
+  }
+}
