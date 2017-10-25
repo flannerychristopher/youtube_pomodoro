d43e51069a08ac33ff55f7351e7a42ed3ff20f1a
flan the dev <flanthedev@gmail.com>
2017-10-24 19:53:29 -0600

    config for heroku

--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,29 @@ var path = require("path");
 
 module.exports = {
   entry: [
-    './src/index.js', 
+    './src/index.js',
   ],
-  
   output: {
     path: path.join(__dirname, "./public"),
     filename: './bundle.js'
   },
-
   module: {
     rules: [
       { test: /\.js$/, loader: 'babel-loader', exclude: '/node_modules/' },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: '/node_modules/' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
     ]
   },
-
   node: {
     fs: 'empty',
     net: 'empty',
     tls: 'empty'
-  }, 
-
+  },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
   }
 };
